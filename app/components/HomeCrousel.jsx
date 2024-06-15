a0ca831b2af4d2eb9c27ad0bd3361f702baee203
Chinaza Okuefuna <79304687+Chinex4@users.noreply.github.com>
2024-06-15 21:22:43 +0100

    Update HomeCrousel.jsx

--- app/components/HomeCrousel.jsx
+++ app/components/HomeCrousel.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import firstImg from '@/public/carousel/1.jpg'
 import secondImg from '@/public/carousel/2.jpg'
 import thirdImg from '@/public/carousel/3.jpg'
 import Image from 'next/image'
 
-const HomeCrousel = () => {
+const HomeCarousel = () => {
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const slides = [firstImg, secondImg, thirdImg];
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
+        }, 2000);
+
+        return () => clearInterval(interval);
+    }, [slides.length]);
+
     return (
         <div className="w-full lg:w-[25rem] carousel rounded-box">
-            <div className="carousel-item w-full">
-                <Image src={firstImg}/>
-            </div>
-            <div className="carousel-item w-full">
-                <Image src={secondImg}/>
-            </div>
-            <div className="carousel-item w-full">
-                <Image src={thirdImg} />
-            </div>
+            {slides.map((slide, index) => (
+                <div key={index} className={`carousel-item w-full ${index === currentSlide ? 'block' : 'hidden'}`}>
+                    <Image src={slide} alt={`Slide ${index + 1}`} />
+                </div>
+            ))}
         </div>
-    )
+    );
 }
 
-export default HomeCrousel
+export default HomeCarousel;
