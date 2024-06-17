d5b2c3081f587d713b1d1d13e2e2ae57fe87d248
Chinaza Okuefuna <79304687+Chinex4@users.noreply.github.com>
2024-06-17 14:38:52 +0100

    Update page.js

--- app/[nftslug]/page.js
+++ app/[nftslug]/page.js
@@ -1,10 +1,7 @@
-"use client";
-
-import { notFound, useRouter } from "next/navigation";
+import { notFound } from "next/navigation";
 import { getNFTData } from "../lib/data";
 import MainHeader from "../components/MainHeader";
-import Image from "next/image";
-import ethcoin from "@/public/coins.png";
+import NFTPage from "../components/NFTPage";
 
 export function generateMetadata({ params }) {
     const nft = getNFTData(params.nftslug);
@@ -19,7 +16,6 @@ export function generateMetadata({ params }) {
 
 export default function Page({ params }) {
     const nft = getNFTData(params.nftslug);
-    const router = useRouter();
 
     if (!nft) {
         notFound();
@@ -28,29 +24,7 @@ export default function Page({ params }) {
     return (
         <div>
             <MainHeader />
-            <div className="mt-32 px-6 lg:px-[4rem] flex flex-col md:flex-row justify-between">
-                <Image
-                    src={nft.image}
-                    alt={nft.creator}
-                    width={400}
-                    height={400}
-                    className="rounded-xl shadow-xl w-full md:w-[300px] lg:w-[400px]"
-                />
-                <div className="md:basis-[50%] lg:basis[60%]">
-                    <h1 className="text-[3rem] font-bold">{nft.creator}</h1>
-                    <p className="text-xl font-semibold flex items-center space-x-2">
-                        <span>{nft.price}ETH</span>
-                        <Image src={ethcoin} width={30} height={30} />
-                    </p>
-                    <p>{nft.description}</p>
-                    <button
-                        onClick={() => router.push('/register/login')}
-                        className="mt-4 bg-primary text-white font-bold py-2 px-4 rounded-lg hover:bg-primary-focus"
-                    >
-                        Buy NFT
-                    </button>
-                </div>
-            </div>
+            <NFTPage nft={nft} />
         </div>
     );
 }
