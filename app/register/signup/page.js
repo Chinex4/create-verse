4d4c1d23479e59a4cc6081b4a0346cb85e841eca
Chinaza Okuefuna <79304687+Chinex4@users.noreply.github.com>
2024-06-17 14:47:43 +0100

    Update page.js

--- app/register/signup/page.js
+++ app/register/signup/page.js
@@ -1,6 +1,6 @@
 export default function Page() {
     return (
-			<div className='w-full md:w-1/2 bg-white p-8 md:p-10 lg:p-16'>
+			<div className='w-full md:w-1/2 bg-white px-6 py-8 md:p-10 lg:p-16'>
 				<h1 className='text-3xl md:text-4xl font-bold'>Sign Up</h1>
 				<form></form>
 			</div>
